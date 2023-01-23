c586c6d4a50ec0276e7005c10edfb3324865b040
Heather <heather.kaymtb@gmail.com>
2023-01-23 13:33:44 +0000

    changing module name in 01b/04, sorting .sh files

--- 01b_1_degree_q/04_create_slope_lut/run_checks_check_filepath.sh
+++
@@ -1,2 +0,0 @@
-singularity exec --bind /bigdata:/bigdata --bind /home/heather:/home/heather /bigdata/heather_gedi/sw_image/au-eoed-dev.sif rsgischkgdalfile.py -i "/bigdata/heather_gedi/data/1_deg_q/4.slope_join/GEDI02_B_2020_Q1/*.gpkg" --vec --rmerr --printerr --epsg 4326 --chkproj
-
--- 01b_1_degree_q/04_create_slope_lut/run_report.sh
+++ 01b_1_degree_q/04_create_slope_lut/run_report.sh
@@ -1,2 +1,2 @@
-singularity exec --bind /bigdata:/bigdata --bind /home/heather:/home/heather /bigdata/heather_gedi/sw_image/au-eoed-dev.sif python gen_jobs.py --report
+singularity exec --bind /bigdata:/bigdata --bind /home/heather:/home/heather /bigdata/heather_gedi/sw_image/au-eoed-dev.sif python identify_lut_tiles.py --report
 
