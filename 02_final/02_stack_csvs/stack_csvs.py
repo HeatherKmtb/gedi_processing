8969b04f1ed49425b99d4931d36265c2bd771117
HeatherKmtb <heather.kaymtb@gmail.com>
2023-02-02 09:21:07 +0000

    removing _ from 02/02

--- 02_final/02_stack_csvs/stack_csvs.py
+++ 02_final/02_stack_csvs/stack_csvs.py
@@ -1,7 +1,7 @@
 import glob
 import pandas as pd
 
-csvs = glob.glob('/bigdata/heather_gedi/results/1_deg/csvs/GEDI_02_B_2020_Q1/*.csv')
+csvs = glob.glob('/bigdata/heather_gedi/results/1_deg/csvs/GEDI02_B_2020_Q1/*.csv')
 out_file = '/bigdata/heather_gedi/results/1_deg/csvs/2020_Q1.csv'
 
 
