567fb734b257cb9da0316da24fe8f08e09b20f83
HeatherKmtb <heather.kaymtb@gmail.com>
2023-06-13 08:30:19 +0100

    adjsuting 01c/03 for different machine

--- 01c_glas/03_split_per_eco/gen_jobs.py
+++ 01c_glas/03_split_per_eco/gen_jobs.py
@@ -37,24 +37,16 @@ class GenCmds(PBPTGenQProcessToolCmds):
 
     def run_gen_commands(self):
         self.gen_command_info(
-            gedi_tiles='/scratch/a.hek4/data/1_deg_q/wwf/1-joined_with_gedi/*.gpkg',
-            out_dir='/scratch/a.hek4/data/1_deg_q/wwf/2-split_per_eco')
+            gedi_tiles='/bigdata/heather_gedi/glas/data/1-joined_with_wwf/*.gpkg',
+            out_dir='/bigdata/heather_gedi/glas/data/2-split_per_eco')
 
 
         self.pop_params_db()
-        self.create_slurm_sub_sh("join_wwf", 8224, '/scratch/a.hek4/logs', run_script="run_exe_analysis.sh",
-                                  db_info_file=None, account_name='scw1403', n_cores_per_job=5, n_jobs=5, job_time_limit='2-23:59',
-                                  module_load='module load parallel singularity\n')
-        #self.create_shell_exe(run_script="run_exe_analysis.sh", cmds_sh_file="cmds_lst.sh", n_cores=25, db_info_file="pbpt_db_info_lcl_file.txt")
+
+        self.create_shell_exe(run_script="run_exe_analysis.sh", cmds_sh_file="cmds_lst.sh", n_cores=25, db_info_file="pbpt_db_info_lcl_file.txt")
 
 if __name__ == "__main__":
     py_script = os.path.abspath("do_tile_analysis.py")
-    script_cmd = "singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif python {}".format(py_script)
-
-    process_tools_mod = 'do_tile_analysis'
-    process_tools_cls = 'DoTileAnalysis'
-
-    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/home/a.hek4/pbpt_db_info.txt",
-                           lock_file_path="/scratch/a.hek4/tmp/gedi_lock_file.txt",
-                           process_tools_mod=process_tools_mod, process_tools_cls=process_tools_cls)
+    script_cmd = "singularity exec --bind /bigdata:/bigdata --bind /home/heather:/home/heather /bigdata/heather_gedi/sw_image/au-eoed-dev.sif python {}".format(py_script)
+    create_tools = GenCmds(cmd=script_cmd, db_conn_file="/bigdata/heather_gedi/pbpt_db_info.txt",lock_file_path="./_lockfile.txt")
     create_tools.parse_cmds()
