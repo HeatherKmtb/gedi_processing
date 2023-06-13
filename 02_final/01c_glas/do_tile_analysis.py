9e2c9df5eec1b5b905cc4511635aa81552b2374e
HeatherKmtb <heather.kaymtb@gmail.com>
2023-06-13 11:28:40 +0100

    changing name_comp numbering in tile_analysis in 02/01c

--- 02_final/01c_glas/do_tile_analysis.py
+++ 02_final/01c_glas/do_tile_analysis.py
@@ -43,8 +43,8 @@ class ProcessJob(PBPTQProcessTool):
         hd, tl = path.split(file)
         shp_lyr_name = path.splitext(tl)[0]
         name_comp = shp_lyr_name.split('_')
-        name = name_comp[1] 
-        eco = name_comp[3]
+        name = name_comp[2] 
+        eco = name_comp[4]
         print(name)
         print(eco)
         
